d568edf38a489963d502941044b10d9792cf001b
505793872 <505793872@qq.com>
2021-04-07 10:34:00 +0800

    screen command.

---
+++ command/screen-command.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set up a new full screen
+screen
+
+# list all screen that opend.
+screen -list
+
+# jump out a full screen
+# ctrl+a+d
+
+# reattach a screen
+screen -r
+screen -x
